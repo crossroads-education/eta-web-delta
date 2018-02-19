2d2fa8ffd955272c94fa81d4991b44ea762b5b51
Alex Hicks <alex@alexhicks.net>
2018-02-19 09:19:20 -0500

    Clean up metadata files

--- static/js/test/index.ts
+++ static/js/test/index.ts
@@ -1,9 +1,9 @@
 import Navigo from "navigo";
 
 $(document).ready(function() {
-    const router = new Navigo(null, false);
+    const router = new Navigo(undefined, false);
 
     router.on("/*", function() {
         $("#root").load("./content");
-        }).resolve();
+    }).resolve();
 });
