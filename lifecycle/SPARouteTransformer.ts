2b3bbd888482bcf953d8067cd7a182bc063571a7
Alex Hicks <alex@alexhicks.net>
2018-03-15 15:27:37 -0400

    Implement support for SPAs to provide `isRoutable()`

--- lifecycle/SPARouteTransformer.ts
+++ lifecycle/SPARouteTransformer.ts
@@ -2,7 +2,10 @@ import * as eta from "../eta";
 import * as querystring from "querystring";
 
 export default class SPARouteTransformer extends eta.LifecycleHandler {
-    private prefixes: string[];
+    private routes: {
+        prefix: string;
+        isRoutable: (path: string) => boolean
+    }[];
 
     public register(): void {
         this.app.on("app:start", this.onAppStart.bind(this));
@@ -11,17 +14,17 @@ export default class SPARouteTransformer extends eta.LifecycleHandler {
 
     private async onAppStart(): Promise<void> {
         // build prefixes array
-        this.prefixes = this.app.getActionsWithFlag("spaRoute", <any>{})
-            .map(a => <string>a.flagValue);
+        this.routes = this.app.getActionsWithFlag("spa", <any>{})
+            .map(a => <any>a.flagValue);
     }
 
     private async onRequest(http: eta.IRequestHandler): Promise<void> {
         if (http.req.headers["x-eta-delta-component"]) return; // don't transform these
         // check if a prefix exists for this request
-        const prefix = this.prefixes.find(p => http.req.mvcPath.startsWith(p + "/"));
-        if (!prefix) return; // if not, ignore this request
+        const route = this.routes.find(r => http.req.mvcPath.startsWith(r.prefix + "/"));
+        if (!route || !route.isRoutable(http.req.mvcPath)) return; // if not, ignore this request
         // redirect with _spaPath parameter (handled on client-side)
-        http.redirect(prefix + "?" + querystring.stringify(eta._.defaults(http.req.query, {
+        http.redirect(route.prefix + "?" + querystring.stringify(eta._.defaults(http.req.query, {
             "_deltaPath": http.req.mvcPath
         })));
     }
