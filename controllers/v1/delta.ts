d4ffa12025a78d1caea5595028ec193d49d86e94
Alex Hicks <alex@alexhicks.net>
2018-03-16 07:33:28 -0400

    Fix formatting, refactor to `GET routes`, and refactor route collection code

--- controllers/v1/delta.ts
+++ controllers/v1/delta.ts
@@ -5,16 +5,13 @@ import * as db from "../../db";
 @eta.mvc.controller()
 export default class DeltaController extends eta.IHttpController {
     @eta.mvc.raw()
-    @eta.mvc.post()
-    public async getRoutes( { basePath }: { basePath: string} ): Promise<void> {
+    @eta.mvc.get()
+    public async routes({ basePath }: { basePath: string}): Promise<void> {
         const routes: string[] = [];
-        // get routes from Eta Application
         Object.keys(this.server.app.viewFiles).forEach(r => {
-            const route: any = r.split("/");
             // check that the route comes from the base directory and is not the base file itself
-            if (route.slice(0, 2).join("/") === basePath && r !== basePath) {
-                // push the route without the base path prefix
-                routes.push("/" + route.slice(2).join("/"));
+            if (r.startsWith(basePath) && r !== basePath) {
+                routes.push(r.substring(basePath.length));
             }
         });
         this.result(db.GenericApiResult.Success, { routes });
