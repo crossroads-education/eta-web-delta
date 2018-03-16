1a81e5b2a2f7f66d90dbc3b804bfb1369f726115
Alex Hicks <alex@alexhicks.net>
2018-03-16 10:35:26 -0400

    Find routes by JS files, not by views

--- controllers/v1/delta.ts
+++ controllers/v1/delta.ts
@@ -8,7 +8,9 @@ export default class DeltaController extends eta.IHttpController {
     @eta.mvc.get()
     public async routes({ basePath }: { basePath: string}): Promise<void> {
         const routes: string[] = [];
-        Object.keys(this.server.app.viewFiles).forEach(r => {
+        Object.keys(this.server.app.staticFiles).forEach(r => {
+            if (!r.endsWith(".js")) return;
+            r = r.replace(/\.js$/, "");
             // check that the route comes from the base directory and is not the base file itself
             if (r.startsWith(basePath) && r !== basePath) {
                 routes.push(r.substring(basePath.length));
