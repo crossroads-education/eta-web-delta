a9537b76485bf74110e3b867a904d5658e7934eb
Alex Hicks <alex@alexhicks.net>
2018-05-04 06:54:59 -0400

    Clean up the route transformer

--- lifecycle/SPARouteTransformer.ts
+++ lifecycle/SPARouteTransformer.ts
@@ -14,16 +14,18 @@ export default class SPARouteTransformer extends eta.LifecycleHandler {
 
     private async onDatabaseConnect(): Promise<void> {
         // build prefixes array
-        this.routes = this.app.getActionsWithFlag("spa", <any>{})
+        this.routes = this.app.getActionsWithFlag("spa", undefined)
             .map(a => <any>a.flagValue);
     }
 
     private async onRequest(http: eta.RequestHandler): Promise<void> {
         if (http.req.headers["x-eta-delta-component"]) return; // don't transform these
-        // check if a prefix exists for this request
-        const route = this.routes.find(r => http.req.mvcPath.startsWith(r.prefix + "/"));
-        if (!route || !route.isRoutable(http.req.mvcPath)) return; // if not, ignore this request
-        // redirect with _spaPath parameter (handled on client-side)
+        // check if a valid route exists for this request
+        const route = this.routes.find(r =>
+            http.req.mvcPath.startsWith(r.prefix + "/") &&
+            r.isRoutable(http.req.mvcPath));
+        if (!route) return; // if not, ignore this request
+        // redirect with _deltaPath parameter (handled on client-side)
         http.redirect(route.prefix + "?" + querystring.stringify(eta._.defaults(http.req.query, {
             "_deltaPath": http.req.mvcPath
         })));
