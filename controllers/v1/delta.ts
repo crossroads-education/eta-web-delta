1fb686e9cb93aab6b2599154d00152762302db82
Alex Hicks <alex@alexhicks.net>
2018-05-07 10:16:35 -0400

    Port to Eta 2.8.1

--- controllers/v1/delta.ts
+++ controllers/v1/delta.ts
@@ -6,9 +6,9 @@ export default class DeltaController extends eta.HttpController {
     async routes(basePath: string) {
         return {
             result: db.GenericApiResult.Success,
-            routes: Object.keys(this.server.app.viewFiles).filter(mvcPath =>
+            routes: Object.keys(this.app.viewFiles).filter(mvcPath =>
                 mvcPath.startsWith(basePath + "/") &&
-                this.server.app.staticFiles[`/js${mvcPath}.js`] !== undefined
+                this.app.staticFiles[`/js${mvcPath}.js`] !== undefined
             ).map(mvcPath => mvcPath.substring(basePath.length))
         };
     }
